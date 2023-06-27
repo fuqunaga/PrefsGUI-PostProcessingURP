47cd5045db529f7a2257e13a00747b96c97894e5
fuqunaga <fukhid@gmail.com>
2023-06-27 19:09:11 +0900

    PrefsVolumeProfile menu order is now the same as VolumeProfile

--- Packages/ga.fuquna.prefspostprocessingurp/RosettaUI/PrefsVolumeProfile-UI.cs
+++ Packages/ga.fuquna.prefspostprocessingurp/RosettaUI/PrefsVolumeProfile-UI.cs
@@ -7,13 +7,27 @@ namespace PrefsGUI.PostProcessingURP
     {
         public virtual Element CreateElement(LabelElement label)
         {
-            return UI.Column(
-                VolumeComponentPrefsList.Select(componentPrefs =>
-                    UI.DynamicElementIf(
-                        () => componentPrefs.Exists,
-                        () => UI.Field(componentPrefs.ComponentType.Name, () => componentPrefs))
+            return UI.DynamicElementOnStatusChanged(
+                () => _volumeProfile == null ? 0 : _volumeProfile.components.Count,
+                _ => UI.Column(
+                    _volumeProfile.components.Select(component =>
+                    {
+                        var componentType = component.GetType();
+                        var prefs = VolumeComponentPrefsList.FirstOrDefault(prefs =>
+                            prefs.ComponentType == componentType);
+                        return prefs == null
+                            ? null
+                            : UI.Field(prefs.ComponentType.Name, () => prefs);
+                    })
                 )
             );
         }
+        // VolumeComponentPrefsList.Select(componentPrefs =>
+                //     UI.DynamicElementIf(
+                //         () => componentPrefs.Exists,
+                //         () => UI.Field(componentPrefs.ComponentType.Name, () => componentPrefs))
+                // )
+        //     );
+        // }
     }
 }
--- Packages/ga.fuquna.prefspostprocessingurp/Scripts/PrefsVolumeProfile.cs
+++ Packages/ga.fuquna.prefspostprocessingurp/Scripts/PrefsVolumeProfile.cs
@@ -4,12 +4,27 @@ namespace PrefsGUI.PostProcessingURP
 {
     public partial class PrefsVolumeProfile
     {
+        private VolumeProfile _volumeProfile;
+    
+    
         public void Bind(VolumeProfile profile)
         {
+            _volumeProfile = profile;
+            
             foreach(var componentPrefs in VolumeComponentPrefsList)
             {
                 componentPrefs.BindVolumeProfile(profile);
             }
+        }
+
+        public void Unbind()
+        {
+            foreach(var componentPrefs in VolumeComponentPrefsList)
+            {
+                componentPrefs.UnbindVolumeProfile();
+            }
+
+            _volumeProfile = null;
         }
         
         public void Reset(VolumeProfile profile)
